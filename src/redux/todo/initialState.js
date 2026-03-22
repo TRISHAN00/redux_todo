d1f4b5c117935290d06a3d716e1f3bcafc8c4104
TRISHAN00 <trishansaha43@gmail.com>
2026-03-22 13:04:17 +0600

    Add: dummy data and created redux files

---
+++ src/redux/todo/initialState.js
@@ -0,0 +1,52 @@
+const initialState = [
+  {
+    id: 1,
+    title: "React Basic",
+    isComplete: true,
+    colors: ["red", "yellow", "green"],
+  },
+  {
+    id: 2,
+    title: "Learn Redux Toolkit",
+    isComplete: false,
+    colors: ["blue"],
+  },
+  {
+    id: 3,
+    title: "Build Todo App",
+    isComplete: false,
+    colors: ["green", "yellow"],
+  },
+  {
+    id: 4,
+    title: "Practice JavaScript ES6",
+    isComplete: true,
+    colors: ["red"],
+  },
+  {
+    id: 5,
+    title: "Learn Next.js",
+    isComplete: false,
+    colors: ["blue", "green"],
+  },
+  {
+    id: 6,
+    title: "Create Portfolio Website",
+    isComplete: false,
+    colors: ["yellow"],
+  },
+  {
+    id: 7,
+    title: "Fix Bugs in Project",
+    isComplete: true,
+    colors: ["red", "blue"],
+  },
+  {
+    id: 8,
+    title: "Deploy App to VPS",
+    isComplete: false,
+    colors: ["green"],
+  },
+];
+
+export default initialState;
